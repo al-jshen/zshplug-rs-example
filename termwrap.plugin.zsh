10c418d8e2685f60c8a1309192b5b288f95243c7
Jeff Shen <jshen2014@hotmail.com>
2020-07-25 10:57:10 -0400

    added comments to zsh plugin file

--- termwrap.plugin.zsh
+++ termwrap.plugin.zsh
@@ -1,6 +1,14 @@
 wrap() {
-    /home/js/programs/termwrap/target/debug/termwrap $2    
+    # pass in $2 to the Rust program and receive its output in "output"
+    output=$(/home/js/programs/termwrap/target/debug/termwrap $2)
+    # potentially do more stuff with it here. 
+    # echo the output
+    echo $output
 }
 
+# makes it so you can use the add-zsh-hook function
 autoload -Uz add-zsh-hook
+
+# adds "wrap" as a preexec function
+# "wrap" will get called before every command you execute
 add-zsh-hook preexec wrap
